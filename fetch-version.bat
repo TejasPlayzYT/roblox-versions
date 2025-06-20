8821015eeae087c483895aa67b36557b8a17cd2a
Chimera Studios <123218812+TejasPlayzYT@users.noreply.github.com>
2025-06-20 13:54:25 -0600

    version fetching and hosting

---
+++ fetch-version.bat
@@ -0,0 +1,4 @@
+@echo off
+python -m roblox_version_checker.py generate-json
+echo("If an error resulted from this, verify your python installation or repair files from the GitHub.")
+pause
--- localserver.bat
+++ localserver.bat
@@ -1,4 +1,5 @@
 @echo off
+start http://localhost:8000
 python -m http.server --directory public 8000
 echo("")
 echo("If you see errors, verify your python installation or install http.server")
